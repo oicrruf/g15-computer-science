9755181846f6ce2aa42798eceaad72865ad35167
Roberto Romero <roberto@ulama.co>
2022-05-10 22:53:34 -0500

    feat: consumir un json

--- ejercicios/pokedex/js/app.js
+++ ejercicios/pokedex/js/app.js
@@ -1,36 +1,22 @@
 let pokemones = [];
 
-//Codigo para ejecutar hasta que la pagina se termina de cargar
-window.onload=function(){
-    document.querySelector("#pokedex").addEventListener("submit",getFormData);
-}
+fetch("https://raw.githubusercontent.com/oicrruf/g15-computer-science/develop/ejercicios/pokedex-registro/json/pokemon.json")
+.then(response => response.json())
+.then(data => {
+    listPokemones(data)
+});
 
-function getFormData(evt){
-    evt.preventDefault();
-    let p_nombre = document.querySelector("#nombre").value;
-    let p_tipo = document.querySelector("#tipo").value;
-    let p_numero = document.querySelector("#numero").value;
-    let p_altura = document.querySelector("#altura").value;
-    let p_peso = document.querySelector("#peso").value;
-    let p_foto = document.querySelector("#foto").value;
 
-    let pokemon = {
-        nombre: p_nombre,
-        tipo: p_tipo,
-        numero: p_numero,
-        altura: p_altura,
-        peso: p_peso,
-        foto:p_foto
-    };
+/*
+fetch("https://raw.githubusercontent.com/oicrruf/g15-computer-science/develop/ejercicios/pokedex-registro/json/pokemon.json")
+.then(function(response){
+    return response.json();
+}).then(function(data){
+    console.log(data);
+});*/
 
-    pokemones.push(pokemon);
-    //Reset del formulario
-    document.querySelector("#pokedex").reset();
-    listPokemones(); 
-    //console.log(pokemones);
-}
 
-function listPokemones(){
+function listPokemones(pokemones){
     document.getElementById("listado").innerHTML="";
 
     for(var i=0;i<pokemones.length;i++){
@@ -39,6 +25,14 @@ function listPokemones(){
 
         let card = document.createElement("div");
         card.classList.add("card", "is-relative");
+        card.dataset.name=pokemones[i].name;
+        card.dataset.id=pokemones[i].id;
+        card.dataset.weight=pokemones[i].weight;
+        card.dataset.height = pokemones[i].height;
+        card.dataset.ThumbnailImage = pokemones[i].ThumbnailImage;
+        card.dataset.number = pokemones[i].number;
+
+        card.addEventListener("click", gritoBatalla)
 
         let cardImage =  document.createElement("div");
         cardImage.classList.add("card-image");
@@ -48,53 +42,47 @@ function listPokemones(){
         //figure.innerHTML='<img src="'+pokemones[i].foto+'">';
     
         let img = document.createElement("img");
-        img.setAttribute("src", pokemones[i].foto);  //img.src = 
+        img.setAttribute("src", pokemones[i].ThumbnailImage);  //img.src = 
         figure.append(img);
 
         cardImage.append(figure);
 
         let cardContent= document.createElement("div");
-        cardContent.classList.add("card-content","p-3");
+        cardContent.classList.add("card-content","px-3","py-4");
         
         let hTitle= document.createElement("h2");
         hTitle.classList.add("is-size-4", "has-text-weight-bold");
-        hTitle.innerText=pokemones[i].nombre;
+        hTitle.innerText=pokemones[i].name;
 
         cardContent.append(hTitle);
 
-        var colorClass= "";
-        switch(pokemones[i].tipo){
-             case "Agua":{
-                colorClass="is-link";
-                break;
-             }
-             case "Fuego":{
-                colorClass="is-danger";
-                break;
-             }
-             case "Planta":{
-                colorClass="is-success";
-                break;
-            }
+        let tipoArreglo=pokemones[i].type;//Arreglo
+        card.dataset.type= pokemones[i].type;
+
+
+        for(var j = 0 ; j<tipoArreglo.length;j++){
+            let tSpan =  document.createElement("span");
+            tSpan.classList.add("tag", "mr-1");
+            tSpan.innerText=tipoArreglo[j];
+            var micolor= getColor(tipoArreglo[j])
+            tSpan.classList.add(micolor);
+            cardContent.append(tSpan);
         }
-
-        let tSpan =  document.createElement("span");
-        tSpan.classList.add("tag", colorClass);
-        tSpan.innerText=pokemones[i].tipo;
-
-        cardContent.append(tSpan);
-
+  
         let p1 = document.createElement("p");
-        p1.innerHTML="<b>No. "+pokemones[i].numero+"</b>";
+        p1.classList.add("pkemn-number");
+        p1.innerHTML="<b>No. "+pokemones[i].number+"</b>";
         cardContent.append(p1);
 
+        /*Para fines graficos quitamos esta parte */ 
+        /*
         let p2 = document.createElement("p");
-        p2.innerHTML="<b>Altura:</b> "+pokemones[i].altura;
+        p2.innerHTML="<b>Altura:</b> "+pokemones[i].height;
         cardContent.append(p2);
 
         let p3 = document.createElement("p");
-        p3.innerHTML="<b>Peso:</b> "+pokemones[i].peso;
-        cardContent.append(p3);
+        p3.innerHTML="<b>Peso:</b> "+pokemones[i].weight;
+        cardContent.append(p3);*/
 
         card.append(cardImage);
         card.append(cardContent);
@@ -104,4 +92,121 @@ function listPokemones(){
         document.getElementById("listado").append(div);
     }
 
+}
+
+function gritoBatalla(evt){
+    //console.log(evt.target);
+    //console.log(evt.currentTarget);
+    //alert(evt.currentTarget.dataset.name);
+
+    var info = evt.currentTarget.dataset
+  
+    document.querySelector("#pokemon-tags").innerHTML='';
+
+    var tipoArreglo = info.type.split(",");
+    for(var j = 0 ; j<tipoArreglo.length;j++){
+        let tSpan =  document.createElement("span");
+        tSpan.classList.add("tag", "mr-1");
+        tSpan.innerText=tipoArreglo[j];
+       //aqui va la funcion
+        var micolor= getColor(tipoArreglo[j])
+        
+        tSpan.classList.add(micolor);
+
+        document.querySelector("#pokemon-tags").append(tSpan);
+    }
+
+    document.querySelector("#pokemon-name").innerHTML= info.name;
+    document.querySelector("#pokemon-thumb").src= info.ThumbnailImage;
+    document.querySelector("#pokemon-number").innerHTML= info.number;
+    document.querySelector("#pokemon-height").innerHTML= info.height;
+    document.querySelector("#pokemon-weight").innerHTML= info.weight;
+
+    //document.querySelector("#popup").classList.add("is-active");
+    //document.querySelector("#popup").style.display='block';
+    document.querySelector("#popup").classList.toggle("is-active");
+}
+
+getColor("water");
+
+function getColor(tipo){
+    var colorClass;
+    switch(tipo){
+        case "grass":{
+           colorClass="grass";
+           break;
+        }
+        case "poison":{
+           colorClass="poison";
+           break;
+        }
+        case "fire":{
+           colorClass="fire";
+           break;
+       }
+       case "water":{
+            colorClass="water";
+            break;
+        }
+        case "psychic":{
+            colorClass="psychic";
+            break;
+        }
+        case "dark":{
+            colorClass="dark";
+            break;
+        }
+        case "bug":{
+            colorClass="bug";
+            break;
+        }
+        case "dragon":{
+            colorClass="dragon";
+            break;
+        }
+        case "electric":{
+            colorClass="electric";
+            break;
+        }
+        case "flying":{
+            colorClass="flying";
+            break;
+        }
+        case "ghost":{
+            colorClass="ghost";
+            break;
+        }
+        case "ground":{
+            colorClass="ground";
+            break;
+        }
+        case "ice":{
+            colorClass="ice";
+            break;
+        }
+        case "rock":{
+            colorClass="rock";
+            break;
+        }
+        case "steel":{
+            colorClass="steel";
+            break;
+        }
+        case "fairy":{
+            colorClass="fairy";
+            break;
+        }
+        default:{
+            colorClass="normal";
+        }
+    }
+    return colorClass;
+}
+
+
+
+window.onload = function(){
+    document.getElementById("delete").addEventListener("click", function(){
+        document.querySelector("#popup").classList.toggle("is-active");
+    });
 }
